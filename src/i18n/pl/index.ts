b0b00e0d854dc0f2888e0a3ba588bd0f9065360b
Szczepan Masny <szczepanmasny@gmail.com>
2019-11-06 02:42:19 +0100

    fix linter error

--- src/i18n/pl/index.ts
+++ src/i18n/pl/index.ts
@@ -90,7 +90,7 @@ export default {
     Heading: {
       buttons: {
         heading: {
-          tooltip: args =>  'Nagłówek ' + args.level
+          tooltip: args => 'Nagłówek ' + args.level
         }
       }
     },
